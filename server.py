ac4b312b49568cf2eaaf953fefeae3da756cf216
procyon7 <bulutr@itu.edu.tr>
2016-10-17 17:53:10 +0300

    Basic features of login page

--- server.py
+++ server.py
@@ -19,6 +19,10 @@ def timeline():
     now =datetime.datetime.now()
     return render_template('timeline.html', current_time=now.ctime())    
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+	
 if __name__ == '__main__':
     VCAP_APP_PORT = os.getenv('VCAP_APP_PORT')
     if VCAP_APP_PORT is not None:
