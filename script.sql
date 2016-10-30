1594b2466d61cdda9444bbc95237b9851c66f099
Halit Uyanık <uyanikhalit1453@gmail.com>
2016-10-30 15:56:38 +0300

    Added notifications TABLE and one INSERT command to script.sql.

--- script.sql
+++ script.sql
@@ -15,6 +15,15 @@ CREATE TABLE IF NOT EXISTS users(
     email text
 );
 
+CREATE TABLE IF NOT EXISTS notifications(
+    notification_id serial primary key,
+    user_id int,
+    notifier_name text,
+    icon text,
+    details text,
+    follow_status int,
+);
+
 
 insert into images (user_id, path, time, text) values (1, '/img1.jpg', now(), 'hello world #1');
 insert into images (user_id, path, time, text) values (1, '/profile1.jpg', now(), 'My profile');
@@ -22,3 +31,5 @@ insert into images (user_id, path, time, text) values (1, '/lovely_cat.jpg', now
 
 insert into users (UserName, Password, photo_path, email) values ('sailormoon', 'abc999', '/photo.jpg', 'sailor@gmail.com' );
 insert into users (UserName, Password, photo_path, email) values ('sunflower', 'defg123', '/photo.jpg', 'sunbb@gmail.com');
+
+insert into notifications(user_id, notifier_name, icon, details, follow_status) values (1,'some_company' ,'/company_icon.jpg', 'Thanks for all followers!' ,0);
